519dc5aebab3dcff2c5ffd91382afaa3cc13a56c
prasannabalajitp <prasannabalaji470@gmail.com>
2024-09-03 18:32:05 +0530

    auth file

--- utils/auth.py
+++ utils/auth.py
@@ -1,11 +1,12 @@
 # auth.py
 from functools import wraps
 from flask import session, redirect, url_for
+from AppConstants.Constants import Constants
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if 'user' not in session:
-            return redirect(url_for('blissmake.login'))
+        if Constants.USER not in session:
+            return redirect(url_for(Constants.BLISSMAKE_LOGIN))
         return f(*args, **kwargs)
     return decorated_function
